702b8cbddccd6cde0752bb83da657b66cb71ce1d
David Litvak Bruno <david.litvakb@gmail.com>
2012-06-02 14:25:51 -0300

    haskell ahora compila

--- haskell/programa.hs
+++ haskell/programa.hs
@@ -1,18 +1,20 @@
 data Term = T | F | If Term Term Term deriving Show
 
-instance Show Term where
-         show T = "true"
-         show If t1 t2 t3 = "(if " ++ show t1
-                            ++ " then " ++ show t2
-                            ++ " else " ++ show t3 ++ ")"
+{-instance Show Term where-}
+        {-show T = "true"-}
+        {-show (If t1 t2 t3) = "(if " ++ show t1-}
+                            {-++ " then " ++ show t2-}
+                            {-++ " else " ++ show t3 ++ ")"-}
 
 eval1 (If T t1 _) = Just t1
 eval1 (If F _ t2) = Just t2
-eval1 (If t1 t2 t3) = Just If t1' t2 t3
-      where Just t1' = eval1 t1
+eval1 (If t1 t2 t3) = Just (If t1' t2 t3)
+        where Just t1' = eval1 t1
 
 eval1 _ = Nothing
 
 eval t = case eval1 t of
-     Just t' -> do putStrLn (show t); eval t'
-     Nothing -> return t
+        Just t' -> do 
+                putStrLn (show t)
+                eval t'
+        Nothing -> return t
