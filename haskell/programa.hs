aeb2192e513d42d5cefe5110652216a17551fc31
David Litvak Bruno <david.litvakb@gmail.com>
2012-06-02 22:20:20 -0300

    mas definiciones en haskell

--- haskell/programa.hs
+++ haskell/programa.hs
@@ -1,10 +1,7 @@
-data Term = T | F | If Term Term Term deriving Show
+import Control.Monad
 
-{-instance Show Term where-}
-        {-show T = "true"-}
-        {-show (If t1 t2 t3) = "(if " ++ show t1-}
-                            {-++ " then " ++ show t2-}
-                            {-++ " else " ++ show t3 ++ ")"-}
+data Term = T | F | If Term Term Term 
+          | Z | Succ Term | Prev Term | IsZero Term
 
 eval1 (If T t1 _) = Just t1
 eval1 (If F _ t2) = Just t2
@@ -15,6 +12,31 @@ eval1 _ = Nothing
 
 eval t = case eval1 t of
         Just t' -> do 
-                putStrLn (show t)
                 eval t'
         Nothing -> return t
+
+data Type = Bool | Nat deriving (Eq)
+
+typeof :: Term -> Maybe Type
+typeof T = Just Bool
+typeof F = Just Bool
+typeof Z = Just Nat
+
+typeof (If c t f) = do
+  Bool <- typeof c
+  ty1  <- typeof t
+  ty2  <- typeof f
+  guard $ ty1 == ty2
+  return ty2
+
+typeof (Succ v) = typeof' (typeof v) where
+    typeof' (Just Nat) = Just Nat
+    typeof' _          = Nothing
+
+typeof (Prev v) = typeof' (typeof v) where
+    typeof' (Just Nat) = Just Nat
+    typeof' _          = Nothing
+
+typeof (IsZero v) = typeof' (typeof v) where
+    typeof' (Just Nat) = Just Bool
+    typeof' _          = Nothing
